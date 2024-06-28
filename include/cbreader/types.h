f01d5777ea27ca48330a9bd0aea21bbbe240b77c
AWildErin <sharpian126@gmail.com>
2024-06-28 21:55:27 +0100

    feat: Initial B3D reader

--- include/cbreader/types.h
+++ include/cbreader/types.h
@@ -21,3 +21,19 @@ struct Triangle
 	std::uint32_t index2 = 0;
 	std::uint32_t index3 = 0;
 };
+
+struct Color
+{
+	float r = 255.f;
+	float g = 255.f;
+	float b = 255.f;
+	float a = 255.f;
+};
+
+struct Quaternion
+{
+	float x = 1.;
+	float y = 1.;
+	float z = 1.;
+	float w = 1.;
+};
